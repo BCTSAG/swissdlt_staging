e048f6fcc4338d39c0eef57bb5e9ebe6fea50428
BCTS <46267529+BCTSAG@users.noreply.github.com>
2021-02-26 17:32:20 +0100

    Update health-check.sh

--- health-check.sh
+++ health-check.sh
@@ -14,7 +14,7 @@ cd ~/swissdlt/
 #~/swissdlt/start-node.sh
 
 #archvie node only
-screen -S node1 geth --datadir node1/ --syncmode 'full' --gcmode=archive  --port 30311 --rpc --rpcaddr 'localhost' --rpcport 8545 --rpcapi 'personal,eth,net,web3,txpool,miner' --networkid 999 --gasprice '1'
+screen -dSm node1 geth --datadir node1/ --syncmode 'full' --gcmode=archive  --port 30311 --rpc --rpcaddr 'localhost' --rpcport 8545 --rpcapi 'personal,eth,net,web3,txpool,miner' --networkid 999 --gasprice '1'
 
 #validating node (don't use this option for unlocking !)
 #screen -dSm node1 geth --datadir node1/ --syncmode 'full' --port 30311 --rpc --rpcaddr 'localhost' --rpccorsdomain "*" --rpcport 8540 --rpcapi 'personal,eth,net,web3,txpool,miner' --networkid 99 --gasprice '1' -unlock 'VALIDATING-ADDRESS-withouth-0x' --password passwort.txt --mine --allow-insecure-unlock
